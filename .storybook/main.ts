1c5ac4ef4c54398d99dbe0af761593ae0d8b885c
SpicyCoder <spk2naveen@hotmail.com>
2024-09-05 13:49:22 +0530

    Add Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,16 @@
+import type { StorybookConfig } from "@storybook/angular";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  framework: {
+    name: "@storybook/angular",
+    options: {},
+  },
+};
+export default config;
---
+++ src/stories/card.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { CardComponent } from '../app/card/card.component';
+
+const meta: Meta<CardComponent> = {
+  title: 'Example/Card',
+  component: CardComponent,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<CardComponent>;
+
+export const Cookies: Story = {
+  args: {
+    title: 'Cookies',
+    content: 'We are using cookies for no reason.',
+    actions: ['Accept', 'Deny'],
+  },
+};
